a2448db417999edd8f50ebfb3e1b9a3d3717c4a6
David Langhorne <david.b.langhorne@gmail.com>
2017-12-09 17:27:12 -0500

    Added basic Foxtrot

--- Foxtrot.py
+++ Foxtrot.py
@@ -1,5 +1,6 @@
 import Step
 import Dance
+import Song
 import arcade
 import pyglet
 
@@ -7,6 +8,33 @@ class Foxtrot(Dance.Dance):
 
     def __init__(self):
         super().__init__()
-        self.beats_per_minute = 120   # BPM should come from song
-        self.seconds_per_beat = 60 / self.beats_per_minute
-        self.name = arcade.create_text("F: Foxtrot", arcade.color.DIM_GRAY, 14)
+        self.name = arcade.create_text("F: Foxtrot", arcade.color.BLACK, 14)
+
+    def basic(self):
+        f = Step.Figure("1: Basic")
+        self.forward_pixels = 210
+        self.side_pixels = 80
+
+        # Step 1 - Forward Slow
+        f.add_leader_step(Step.Forward(Step.Foot.LEFT, self.forward_pixels, self.seconds_per_beat * 2))
+        f.add_follower_step(Step.Backward(Step.Foot.RIGHT, self.forward_pixels, self.seconds_per_beat * 2))
+
+        # Step 2 - Forward Slow
+        f.add_leader_step(Step.Forward(Step.Foot.RIGHT, self.forward_pixels, self.seconds_per_beat * 2))
+        f.add_follower_step(Step.Backward(Step.Foot.LEFT, self.forward_pixels, self.seconds_per_beat * 2))
+
+        # Step 3 - Side Quick
+        f.add_leader_step(Step.Side(Step.Foot.LEFT, -self.side_pixels, self.seconds_per_beat))
+        f.add_follower_step(Step.Side(Step.Foot.RIGHT, self.side_pixels, self.seconds_per_beat))
+
+        # Step 4 - Close the feet
+        f.add_leader_step(Step.Close(Step.Foot.RIGHT, self.seconds_per_beat))
+        f.add_follower_step(Step.Close(Step.Foot.LEFT, self.seconds_per_beat))
+
+        return f
+
+    def load_figures(self):
+        self.figure_list.append(self.basic())
+
+    def load_songs(self):
+        self.song_list.append(Song.Song(1, "Music/Foxtrot/The Pink Panther Theme.mp3", 116))
